6b86db42154bca19c8b4a311e82af456e3260c3e
amzmohammad <{Amz1990}>
2019-05-20 17:11:47 +0430

    third

--- simpleappriori.py
+++ simpleappriori.py
@@ -9,12 +9,30 @@ C1=[]
 min_sup=2
 L=[]
 
+
+def divide(iterable, parts):
+    ''' Partitions an iterable into parts number of lists. '''
+    items = list(iterable)
+
+    seqs = [[] for _ in range(parts)]
+    while items:
+        for i in range(parts):
+            if not items:
+                break
+
+            seqs[i].append(items.pop())
+
+    return seqs
+
+def Subtract(lst,itm):
+    return list(set(lst) - set([itm]))
+
 def Join(Cx,Cy):
     Cres=[]
     for lstx in Cx:
         for lsty in Cy:
             newlst=lstx + [x for x in lsty if x not in lstx]
-            if(newlst not in Cx and newlst not in Cy and newlst not in Cres):
+            if(newlst not in Cx+Cy and sorted(newlst) not in Cres):
                 Cres.append(sorted(newlst))
 
     return Cres
@@ -44,16 +62,16 @@ print(C1)
 C=C1
 
 i=1
-while(i<3):
+while(True):
     print("Iter #"+str(i))
-    C_temp=C
-    for itemlst in C_temp:
-        print(str(itemlst) + " : " + str(Support(itemlst)))
-        if(Support(itemlst)<min_sup):
-            C_temp.remove(itemlst)
+    C_temp=[]
+    for itemlst in C:
+        if(Support(itemlst)>=min_sup):
+            print(str(itemlst) + " : " + str(Support(itemlst)))
+            C_temp.append(itemlst)
 
-    print(C_temp)
-    L.append(C_temp)
+    #print(C_temp)
+    L=L+C_temp
 
     C=[]
     #for itemlst_x in C_temp:
@@ -61,4 +79,20 @@ while(i<3):
     #        C.append(np.concatenate(itemlst_x,itemlst_x))
 
     C=Join(C_temp,C_temp)
+    if(len(C)==0):
+        print("Finish")
+        break;
     i=i+1
+
+print("L:")
+print(L)
+
+
+
+for itemlst in L:
+    allperms=[itemlst[i::2] for i in range(2)]
+    print(divide(itemlst,2))
+    if(len(itemlst)>=3):
+        for item in itemlst:
+            print(str(Subtract(itemlst,item))+" ----> "+ str(item) + "   Confidence :" + str(Support(itemlst)/Support(Subtract(itemlst,item))))
+
